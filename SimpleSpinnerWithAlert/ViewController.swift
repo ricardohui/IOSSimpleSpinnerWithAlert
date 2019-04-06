89deff74c143b138eda109635f373a9950417eef
Ricardo Hui <eehaha@gmail.com>
2019-04-06 08:05:01 +0800

    Spinner and Alert completed

--- SimpleSpinnerWithAlert/ViewController.swift
+++ SimpleSpinnerWithAlert/ViewController.swift
@@ -9,7 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBAction func showAlert(_ sender: Any) {
+        print("alert button pressed")
+        let alertController = UIAlertController(title: "Hey there!", message: "Are you sure?", preferredStyle: UIAlertController.Style.alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: { (action) in
+            print("OK button pressed")
+            self.dismiss(animated: true, completion: nil)
+        }))
+        
+        alertController.addAction(UIAlertAction(title: "No", style: .default, handler: { (action) in
+            print("No button pressed")
+            self.dismiss(animated: true
+                , completion: nil)
+        }))
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
+    @IBAction func pauseApp(_ sender: Any) {
+        
+        let activityIndicator = UIActivityIndicatorView(frame: CGRect(x: 0, y: 0, width: 50, height: 60))
+        activityIndicator.center = self.view.center
+        activityIndicator.hidesWhenStopped = true
+        activityIndicator.style = UIActivityIndicatorView.Style.gray
+        view.addSubview(activityIndicator)
+        activityIndicator.startAnimating()
+        // to pause the app so that user cannot interact the UI until something resolved
+        UIApplication.shared.beginIgnoringInteractionEvents()
+        
+        //unlock the UI when somethign resolved
+        UIApplication.shared.endIgnoringInteractionEvents()
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
